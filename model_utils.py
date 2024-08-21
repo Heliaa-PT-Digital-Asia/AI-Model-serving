2be2a74b871411a3775bc1312939743f689a9023
Sajad Sajadpour <46099808+SajadSajadpour@users.noreply.github.com>
2024-08-21 17:04:55 +0800

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -2,17 +2,43 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import boto3
-from botocore.exceptions import NoCredentialsError
+# from botocore.exceptions import NoCredentialsError
 
-def load_model_from_s3(bucket_name, model_key, local_path='local_model'):
+# def load_model_from_s3(bucket_name, model_key, local_path='local_model'):
+#     s3 = boto3.client('s3')
+#     try:
+#         s3.download_file(bucket_name, model_key, local_path)
+#         print("Model downloaded successfully from S3.")
+#     except NoCredentialsError:
+#         print("Credentials not available.")
+#         return None
+#     return tf.saved_model.load(local_path)
+
+from botocore.exceptions import NoCredentialsError, ClientError
+
+def load_model_from_s3(bucket_name, model_key, local_path):
     s3 = boto3.client('s3')
+    s3_path = f"s3://{bucket_name}/{model_key}"
+
     try:
+        print(f"Attempting to download model from {s3_path}...")
         s3.download_file(bucket_name, model_key, local_path)
-        print("Model downloaded successfully from S3.")
+        print(f"Model downloaded successfully from {s3_path} to {local_path}.")
     except NoCredentialsError:
         print("Credentials not available.")
         return None
-    return tf.saved_model.load(local_path)
+    except ClientError as e:
+        print(f"Failed to download model from {s3_path}. Error: {e}")
+        return None
+
+    # Load the TensorFlow model from the local path
+    try:
+        model = tf.saved_model.load(local_path)
+        print(f"Model loaded successfully from {local_path}.")
+        return model
+    except Exception as e:
+        print(f"Failed to load model from {local_path}. Error: {e}")
+        return None
 
 def run_inference(model, frame):
     movenet = model.signatures['serving_default']
